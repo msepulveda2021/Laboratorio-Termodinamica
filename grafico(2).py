3f50ae69eb8e701eba8c0ce45f0d5f0d5ff65f50
Martin Sepulveda <80585542+msepulveda2021@users.noreply.github.com>
2022-09-07 17:56:02 -0300

    Fix axis

--- grafico(2).py
+++ grafico(2).py
@@ -17,19 +17,19 @@ y_250 = np.array([10,5,7.5,12.5,15])
 
 #----------------GRAFICOS----------------------------------------------------------------------
 
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(5,7), constrained_layout=True)
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(11,6), constrained_layout=True)
 
-ax1.plot(x_50, y_50, marker='0')
+ax1.plot(x_50, y_50, marker='o')
 ax1.set_xlabel('Temperatura (K)')
 ax1.set_ylabel('Volumen (nm)')
 ax1.set_title('Simulación 2')
 
-ax2.plot(x_150, y_150, marker='0')
+ax2.plot(x_150, y_150, marker='o')
 ax2.set_xlabel('Temperatura (K)')
 ax2.set_ylabel('Volumen (nm)')
 ax2.set_title('Simulación 3')
 
-ax3.plot(x_250, y_250, marker='0')
+ax3.plot(x_250, y_250, marker='o')
 ax3.set_xlabel('Temperatura (K)')
 ax3.set_ylabel('Volumen (nm)')
 ax3.set_title('Simulación 4')
