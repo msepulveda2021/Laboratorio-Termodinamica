d15aaeb66293328ac6f53bd5d973331b956eb30c
Martin Sepulveda <80585542+msepulveda2021@users.noreply.github.com>
2022-09-07 13:50:44 -0300

    bug fix

--- grafico(2).py
+++ grafico(2).py
@@ -1,5 +1,5 @@
 import numpy as np
-import matpltlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 #----------------DATOS-------------------------------------------------------------------------
 
