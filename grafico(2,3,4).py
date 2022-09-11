285a02b3a28e9c1b56e12dde5fdf49c51e075ebb
martin sepulveda <martinsep1000@gmail.com>
2022-09-11 14:27:04 -0300

    Ahora si jijiji

--- grafico(2,3,4).py
+++ grafico(2,3,4).py
@@ -17,7 +17,8 @@ y_250 = np.array([10,5,7.5,12.5,15])
 
 #----------------GRAFICOS----------------------------------------------------------------------
 
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(11,6), constrained_layout=True)
+plt.figure()
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(11,5), constrained_layout=True)
 
 ax1.plot(x_50, y_50, marker='o', color='mediumaquamarine', label='n=50, presión 5.8 atm')
 ax1.set_xlabel('Temperatura (K)')
@@ -27,15 +28,13 @@ ax1.legend()
 
 ax2.plot(x_150, y_150, marker='o', color='turquoise', label='n=150, presión 18 atm')
 ax2.set_xlabel('Temperatura (K)')
-ax2.set_ylabel('Volumen (nm)')
 ax2.set_title('Simulación 3')
 ax2.legend()
 
 ax3.plot(x_250, y_250, marker='o', color='lightseagreen', label='n=250, presión 29.2 atm')
 ax3.set_xlabel('Temperatura (K)')
-ax3.set_ylabel('Volumen (nm)')
 ax3.set_title('Simulación 4')
 ax3.legend()
 
-plt.show()
 plt.savefig('grafico(2,3,4).pdf')
+plt.show()
